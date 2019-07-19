f06453127466ad514ab8ec6cd949cb46499c0782
Barry Lee <bclee@buffalo.edu>
2019-07-19 12:57:06 -0400

    Building init_players
    Adding player count, player user names, and checking if they are all unique. Prints out everyone's name at the end.

--- init_players.py
+++ init_players.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class input_conditions(Enum):
 	PLAYER_COUNT = 1
+	USER_NAME = 2
 
 def validate_input(input_txt, condition, err_msg):
 	user_input = input(input_txt)
@@ -13,6 +14,13 @@ def validate_input(input_txt, condition, err_msg):
 				user_input = input(input_txt)
 			else:
 				break
+
+		if condition == input_conditions.USER_NAME:
+			if not user_input.isalnum() or len(user_input) > 10 or len(user_input) < 1:
+				print(err_msg)
+				user_input = input(input_txt)
+			else:
+				break
 		else:
 			break
 
@@ -20,3 +28,17 @@ def validate_input(input_txt, condition, err_msg):
 
 
 player_count = validate_input("Enter player count: ", input_conditions.PLAYER_COUNT, "Please enter a valid number between 2 and 5")
+player_count = int(player_count)
+
+players = []
+for i in range(1, player_count+1):
+	player_name = validate_input("Enter player "+str(i)+"'s name: ", input_conditions.USER_NAME, "Please enter an alphanumeric string less than 10 characters")
+	
+	while player_name in players:
+		print("Players cannot have the same name as existing players. Think harder and be unique.")
+		player_name = validate_input("Enter player "+str(i)+"'s name: ", input_conditions.USER_NAME, "Please enter an alphanumeric string less than 10 characters")
+	players.append(player_name)
+
+print("\n\nAll set! Here are the players!")
+for i in range(1, player_count+1):
+	print("Player "+ str(i) +": "+players[i-1])
