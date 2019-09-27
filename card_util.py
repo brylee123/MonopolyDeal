88f44d13f925e0aaf9e555695695b41a73464ff0
Barry Lee <bclee@buffalo.edu>
2019-09-27 12:03:19 -0400

    card_util rename and restructure.
    Renamed deck_util to card_util. Fixed action card printing. Moved card checking to card_util.

---
+++ card_util.py
@@ -0,0 +1,68 @@
+import init_deck
+
+def draw2(game_deck):
+	# Use for start of each turn and pass go
+	card1 = game_deck.pop()
+	card2 = game_deck.pop()
+
+	return card1, card2
+
+def draw5(game_deck):
+	# Use for dealing in the beginning and if player has no cards
+	card1 = game_deck.pop()
+	card2 = game_deck.pop()
+	card3 = game_deck.pop()
+	card4 = game_deck.pop()
+	card5 = game_deck.pop()
+
+	return card1, card2, card3, card4, card5
+
+def display_card(n):
+	card = init_deck.get_card(n)
+	card_details = 0
+	print(type(card).__name__) # Class Name
+	print(vars(card)) # Object Attributes/Member Variables
+
+	information = ""	# Add print values of each card.
+
+	# If action_card
+	if isinstance(card, init_deck.action_card):
+		information = "[$"+str(card.value)+"] " + card.name + ": " + card.description
+
+	# If property_card
+	elif isinstance(card, init_deck.property_card):
+
+		property_details1 = card.property1.color.value
+
+		if card.wild:
+			print("Wild Card. This card can be used as part of any property set. This card has no monetary value.")
+			return
+
+		rent1 = ", ".join([str(i) for i in card.property1.rent]) 
+		property_details1 = property_details1 + " (" + rent1 + ")"
+		information = "[$"+str(card.value)+"] " + card.name + ": " + property_details1
+		
+		if card.property2 != None:
+			property_details2 = card.property2.color.value
+			rent2 = ", ".join([str(i) for i in card.property2.rent])
+			property_details2 = property_details2 + " (" + rent2 + ")"
+
+			information = "[$"+str(card.value)+"] " + card.name + ": " + property_details1 + " and " + property_details2
+
+	# If rent_card
+	elif isinstance(card, init_deck.rent_card):
+		rentcolor1 = card.color.pop().value
+		rentcolor2 = ""
+		if len(card.color) > 0:
+			rentcolor2 = card.color.pop().value
+		information = "[$"+str(card.value)+"] " + "Rent!: "+ rentcolor1
+
+		if len(rentcolor2) > 0:
+			information += " or " + rentcolor2
+
+	# If money_card
+	elif isinstance(card, init_deck.money_card):
+		information = "[$"+str(card.value)+"] "
+
+	print(information)
+	return
